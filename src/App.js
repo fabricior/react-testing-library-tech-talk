0ecb6b07e1131d772c1772ff827d50b1cf4aa572
Fabricio Rateni <fabriciorateni@gmail.com>
2020-05-01 19:39:04 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React Testing Library Hands On
+      </header>        
+      <section className="App-section, new-message-section">        
+      </section>
+    </div>
+  );
+}
+
+export default App;
