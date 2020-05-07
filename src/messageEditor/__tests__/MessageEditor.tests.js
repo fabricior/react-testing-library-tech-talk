ca82884ae98b326f23fafe8d824f1a5afc269b09
Fabricio <fabricio.rateni@endava.com>
2020-05-06 23:30:52 -0300

    removing unneeded annotations

--- src/messageEditor/__tests__/MessageEditor.tests.js
+++ src/messageEditor/__tests__/MessageEditor.tests.js
@@ -6,23 +6,21 @@ import MessageEditor from "../MessageEditor";
 test.skip("can submit a new message", () => {
     render(<MessageEditor />)
     const input = screen.getByLabelText(/message:/i);
-     
-    const submitMessageButton = screen.getByRole('button'); // ❌ fails because there is more than one button.
 
-    // 💡 submitMessageButton should be disabled by default.
-    expect(submitMessageButton).___your_code_here___();
+    const submitMessageButton = screen.getByRole('button', { name: "Submit" });
+
+    expect(submitMessageButton).not.toBeEnabled();
 
     user.type(input, 'hello everyone');
 
-    // 💡 submitMessageButton should be enabled now.
-    expect(submitMessageButton).___your_code_here___();
+    expect(submitMessageButton).toBeEnabled();
 });
 
 test.skip("new messages are diplayed on the list and status message is updated", () => {
     render(<MessageEditor />)
     const input = screen.getByLabelText(/message:/i);
 
-    const submitMessageButton = screen.getByRole('button'); // ❌ still fails but you can copy fixed code by now.
+    const submitMessageButton = screen.getByRole('button', { name: "Submit" });
 
     user.type(input, 'hello everyone');
     user.click(submitMessageButton);
@@ -42,16 +40,13 @@ test.skip("focused items and tab order works as expected", () => {
 
     const input = screen.getByLabelText(/message:/i);
 
-    // 💡 input should have focus now.
-    expect(input).___your_code_here___();
+    expect(input).toHaveFocus();
 
-    const submitMessageButton = screen.getByRole('button'); // ❌ still fails but you can copy fixed code by now.
+    const submitMessageButton = screen.getByRole('button', { name: "Submit" });
 
-    // 💡 submitMessageButton don't now.
-    expect(submitMessageButton).___your_code_here___();
+    expect(submitMessageButton).not.toHaveFocus();
 
     user.tab();
 
-    // 💡 submitMessageButton have focus now.
-    expect(submitMessageButton).___your_code_here___();
+    expect(submitMessageButton).toHaveFocus();
 });
